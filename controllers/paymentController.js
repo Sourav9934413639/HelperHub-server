ad5aea50bff4d58b74dd2722231b060f6897250e
Sourav Sinha <sourav9934413639@gmail.com>
2024-03-31 13:50:06 +0530

    changed the name of environment variable

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -34,7 +34,7 @@ export const checkout=catchAsyncErrors(async(req,res)=>{
         user:req.user,
         
     })
-    res.redirect(`${process.env.BASE_URL}/paymentsuccess?reference=${razorpay_payment_id}`)
+    res.redirect(`${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`)
   }
   else{
     res.status(400).json({
